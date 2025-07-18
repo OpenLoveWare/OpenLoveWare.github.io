32feea0688761fd16881e27e395f2257b0051dd3
Augustin Ambiehl <ambiehlaugustin@gmail.com>
2025-07-18 15:36:12 +0200

    autocommit

--- pico/index.service.worker.js
+++ pico/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752842406|4918753674';
+const CACHE_VERSION = '1752845761|8273605080';
 /** @type {string} */
 const CACHE_PREFIX = 'Piko-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
